f9003982365f58ed5a033e8871305810b3425c07
ulises-jeremias <ulisescf.24@gmail.com>
2020-10-14 01:11:40 -0300

    Updated blas and la

--- blas/blas.v
+++ blas/blas.v
@@ -16,7 +16,7 @@ pub struct Workspace {
 }
 
 pub fn allocate_workspace(size int) Workspace {
-	ptr := &f64(v_calloc(size * int(sizeof(f64))))
+	ptr := &f64(v_calloc(size * int(int(sizeof(f64)))))
 	return Workspace{
 		size: size
 		work: ptr
@@ -26,7 +26,8 @@ pub fn allocate_workspace(size int) Workspace {
 pub fn fortran_view_or_copy(t num.NdArray) num.NdArray {
 	if t.flags.fortran {
 		return t.view()
-	} else {
+	}
+	else {
 		return t.copy('F')
 	}
 }
@@ -47,22 +48,22 @@ pub fn assert_matrix(a num.NdArray) {
 	}
 }
 
-pub fn ddot(a, b num.NdArray) f64 {
+pub fn ddot(a num.NdArray, b num.NdArray) f64 {
 	if a.ndims != 1 || b.ndims != 1 {
 		panic('Tensors must be one dimensional')
-	} else if a.size != b.size {
+	}
+	else if a.size != b.size {
 		panic('Tensors must have the same shape')
 	}
 	return C.cblas_ddot(a.size, a.buffer(), a.strides[0], b.buffer(), b.strides[0])
 }
 
-pub fn dger(a, b num.NdArray) num.NdArray {
+pub fn dger(a num.NdArray, b num.NdArray) num.NdArray {
 	if a.ndims != 1 || b.ndims != 1 {
 		panic('Tensors must be one dimensional')
 	}
 	out := num.empty([a.size, b.size])
-	blas.dger(a.size, b.size, 1.0, a.buffer(), a.strides[0], b.buffer(), b.strides[0],
-		out.buffer(), out.shape[1])
+	C.cblas_dger(blas.lapack_row_major, a.size, b.size, 1.0, a.buffer(), a.strides[0], b.buffer(), b.strides[0], out.buffer(), out.shape[1])
 	return out
 }
 
@@ -79,8 +80,7 @@ pub fn dlange(a num.NdArray, norm byte) f64 {
 	}
 	m := fortran_view_or_copy(a)
 	work := &f64(v_calloc(m.shape[0] * int(sizeof(f64))))
-	return C.LAPACKE_dlange(&norm, &m.shape[0], &m.shape[1], m.buffer(), &m.shape[0],
-		work)
+	return C.LAPACKE_dlange(&norm, &m.shape[0], &m.shape[1], m.buffer(), &m.shape[0], work)
 }
 
 pub fn dpotrf(a num.NdArray, uplo byte) num.NdArray {
@@ -88,13 +88,18 @@ pub fn dpotrf(a num.NdArray, uplo byte) num.NdArray {
 		panic('Tensor must be two-dimensional')
 	}
 	ret := a.copy('F')
-        mut ret_buffer := &f64(ret.buffer())
-	blas.dpotrf(uplo == `U`, ret.shape[0], mut ret_buffer, ret.shape[0])
+	info := 0
+	C.LAPACKE_dpotrf(&uplo, &ret.shape[0], ret.buffer(), &ret.shape[0], &info)
+	if info > 0 {
+		panic('Tensor is not positive definite')
+	}
 	if uplo == `U` {
 		num.triu_inpl(ret)
-	} else if uplo == `L` {
+	}
+	else if uplo == `L` {
 		num.tril_inpl(ret)
-	} else {
+	}
+	else {
 		panic('Invalid option provided for UPLO')
 	}
 	return ret
@@ -106,8 +111,10 @@ pub fn det(a num.NdArray) f64 {
 	n := a.shape[1]
 	ipiv := &int(v_calloc(int(sizeof(int)) * n))
 	info := 0
-        mut ret_buffer := &f64(ret.buffer())
-	blas.dgetrf(m, n, mut ret_buffer, m, ipiv)
+	C.LAPACKE_dgetrf(&m, &n, ret.buffer(), &m, ipiv, &info)
+	if info > 0 {
+		panic('Singular matrix')
+	}
 	ldet := num.prod(ret.diagonal())
 	mut detp := 1
 	for i := 0; i < n; i++ {
@@ -125,27 +132,37 @@ pub fn inv(a num.NdArray) num.NdArray {
 	ret := a.copy('F')
 	n := a.shape[0]
 	ipiv := &int(v_calloc(n * int(sizeof(int))))
-	info := 0
-        mut ret_buffer := &f64(ret.buffer())
-	blas.dgetrf(n, n, mut ret_buffer, n, ipiv)
+	mut info := 0
+	C.LAPACKE_dgetrf(&n, &n, ret.buffer(), &n, ipiv, &info)
+	if info > 0 {
+		panic('Singular matrix')
+	}
 	lwork := n * n
 	work := &f64(v_calloc(lwork * int(sizeof(f64))))
-	C.LAPACKE_dgetri(&n, ret_buffer, &n, ipiv, work, &lwork, &info)
+	info = C.LAPACKE_dgetri(&n, ret.buffer(), &n, ipiv, &work)
+        if info != 0 {
+                panic('lapack failed')
+        }
 	return ret
 }
 
-pub fn matmul(a, b num.NdArray) num.NdArray {
+pub fn matmul(a num.NdArray, b num.NdArray) num.NdArray {
 	dest := num.empty([a.shape[0], b.shape[1]])
 	ma := match (a.flags.contiguous) {
-		true { a }
-		else { a.copy('C') }
+		true{
+			a
+		}
+		else {
+			a.copy('C')}
 	}
 	mb := match (b.flags.contiguous) {
-		true { b }
-		else { b.copy('C') }
+		true{
+			b
+		}
+		else {
+			b.copy('C')}
 	}
-	blas.dgemm(.no_trans, .no_trans, ma.shape[0], mb.shape[1], ma.shape[1], 1.0, ma.buffer(),
-		ma.shape[1], mb.buffer(), mb.shape[1], 1.0, dest.buffer(), dest.shape[1])
+	C.cblas_dgemm(blas.lapack_row_major, blas.c_trans(false), blas.c_trans(false), ma.shape[0], mb.shape[1], ma.shape[1], 1.0, ma.buffer(), ma.shape[1], mb.buffer(), mb.shape[1], 1.0, dest.buffer(), dest.shape[1])
 	return dest
 }
 
@@ -158,8 +175,7 @@ pub fn eigh(a num.NdArray) []num.NdArray {
 	uplo := `L`
 	info := 0
 	workspace := allocate_workspace(3 * n - 1)
-	C.LAPACKE_dsyev(jobz, uplo, n, ret.buffer(), n, w.buffer(), workspace.work, workspace.size,
-		&info)
+	C.LAPACKE_dsyev(&jobz, &uplo, &n, ret.buffer(), &n, w.buffer(), workspace.work)
 	if info > 0 {
 		panic('Failed to converge')
 	}
@@ -175,9 +191,13 @@ pub fn eig(a num.NdArray) []num.NdArray {
 	vl := num.allocate_cpu([n, n], 'F')
 	vr := vl.copy('C')
 	workspace := allocate_workspace(n * 4)
+	jobvr := `V`
+	jobvl := `V`
 	info := 0
-	blas.dgeev(true, true, n, ret.buffer(), n, mut wr.buffer(), wl.buffer(), vl.buffer(),
-		n, vr.buffer(), n, workspace.work, workspace.size)
+	C.LAPACKE_dgeev(&jobvl, &jobvr, &n, ret.buffer(), &n, wr.buffer(), wl.buffer(), vl.buffer(), &n, vr.buffer(), &n, workspace.work)
+	if info > 0 {
+		panic('QR algorithm failed')
+	}
 	return [wr, vl]
 }
 
@@ -190,8 +210,7 @@ pub fn eigvalsh(a num.NdArray) num.NdArray {
 	info := 0
 	w := num.empty([n])
 	workspace := allocate_workspace(3 * n - 1)
-	C.LAPACKE_dsyev(&jobz, &uplo, &n, ret.buffer(), &n, w.buffer(), workspace.work, &workspace.size,
-		&info)
+	C.LAPACKE_dsyev(&jobz, &uplo, &n, ret.buffer(), &n, w.buffer(), workspace.work)
 	if info > 0 {
 		panic('Failed to converge')
 	}
@@ -207,13 +226,17 @@ pub fn eigvals(a num.NdArray) num.NdArray {
 	vl := num.allocate_cpu([n, n], 'F')
 	vr := vl.copy('C')
 	workspace := allocate_workspace(n * 3)
+	jobvr := `N`
+	jobvl := `N`
 	info := 0
-	blas.dgeev(false, false, n, ret.buffer(), n, wr.buffer(), wl.buffer(), vl.buffer(),
-		n, vr.buffer(), n, workspace.work, workspace.size)
+	C.LAPACKE_dgeev(&jobvl, &jobvr, &n, ret.buffer(), &n, wr.buffer(), wl.buffer(), vl.buffer(), &n, vr.buffer(), &n, workspace.work)
+	if info > 0 {
+		panic('QR algorithm failed')
+	}
 	return wr
 }
 
-pub fn solve(a, b num.NdArray) num.NdArray {
+pub fn solve(a num.NdArray, b num.NdArray) num.NdArray {
 	assert_square_matrix(a)
 	af := fortran_view_or_copy(a)
 	bf := b.copy('F')
@@ -224,7 +247,7 @@ pub fn solve(a, b num.NdArray) num.NdArray {
 	}
 	ipiv := &int(v_calloc(n * int(sizeof(int))))
 	info := 0
-	blas.dgesv(n, m, af.buffer(), n, ipiv, bf.buffer(), m)
+	C.LAPACKE_dgesv(&n, &m, af.buffer(), &n, ipiv, bf.buffer(), &m, &info)
 	return bf
 }
 
@@ -240,11 +263,10 @@ pub fn hessenberg(a num.NdArray) num.NdArray {
 	ihi := 0
 	job := `B`
 	info := 0
-	C.LAPACKE_dgebal(job, n, ret.buffer(), n, ilo, ihi, s.buffer(), &info)
+	C.LAPACKE_dgebal(&job, &n, ret.buffer(), &n, &ilo, &ihi, s.buffer(), &info)
 	tau := num.empty([n])
 	workspace := allocate_workspace(n)
-	C.LAPACKE_dgehrd(n, ilo, ihi, ret.buffer(), n, tau.buffer(), workspace.work, workspace.size,
-		&info)
+	C.LAPACKE_dgehrd(&n, &ilo, &ihi, ret.buffer(), &n, tau.buffer(), workspace.work)
 	num.triu_inpl_offset(ret, -1)
 	return ret
 }
