d302a4398b8ddfbf0760c18b52827684761685ca
ulises-jeremias <ulisescf.24@gmail.com>
2020-10-25 11:52:11 -0300

    Updated fmt

--- nn/nn_test.v
+++ nn/nn_test.v
@@ -2,7 +2,8 @@ import vnum.nn
 import vnum.num
 
 fn xor_test() {
-        expected := num.from_f64_2d([[0.0387387], [0.976217], [0.976216], [0.00880164]])
+	expected := num.from_f64_2d([[0.0387387],
+		[0.976217], [0.976216], [0.00880164]])
 	features := num.from_int([0, 0, 0, 1, 1, 0, 1, 1], [4, 2])
 	labels := num.from_int([0, 1, 1, 0], [4, 1])
 	mut m := nn.new(0.7, 10000, 3, .sigmoid)
