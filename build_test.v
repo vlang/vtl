a0fe71b5f6c25d35941874b83199ce9849117148
ulises-jeremias <ulisescf.24@gmail.com>
2020-11-14 23:40:42 -0300

    Added convertor

--- build_test.v
+++ build_test.v
@@ -1,7 +1,12 @@
 module vtl
 
 fn test_new() {
+        init := 1.0
 	t := new_tensor<f64>({
 		shape: [3]
+                init: &init
 	})
+        varray := tensor_to_varray<f64>(t)
+        assert varray.len == 3
+        assert varray[0] == 1.0
 }
--- storage_test.v
+++ storage_test.v
@@ -1,9 +1,13 @@
 module vtl
 
 fn test_cpu_storage_with_default() {
+        init := 1.0
 	s := new_storage<f64>({
 		len: 2
-		init: voidptr(0)
+		init: &init
 		strategy: .cpu
 	})
+        varray := storage_to_varray<f64>(s)
+        assert varray.len == 2
+        assert varray[0] == 1.0
 }
