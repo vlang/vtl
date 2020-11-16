1b422bc3f4c955041fea19e702e4399b14d8d757
ulises-jeremias <ulisescf.24@gmail.com>
2020-11-15 22:58:26 -0300

    Added helpers

--- util.v
+++ util.v
@@ -24,3 +24,31 @@ fn size_from_shape(shape []int) int {
 	}
 	return accum
 }
+
+// assert_shape_off_axis ensures that the shapes of Tensors match
+// for concatenation, except along the axis being joined
+fn assert_shape_off_axis(ts []Tensor, axis int, shape []int) []int {
+	mut retshape := shape.clone()
+	for t in ts {
+		if t.shape.len != retshape.len {
+			panic('All inputs must share the same number of axes')
+		}
+		mut i := 0
+		for i < shape.len {
+			if (i != axis) && (t.shape[i] != shape[i]) {
+				panic('All inputs must share a shape off axis')
+			}
+			i++
+		}
+		retshape[axis] += t.shape[axis]
+	}
+	return retshape
+}
+
+fn assert_shape(shape []int, ts []Tensor) {
+	for t in ts {
+		if shape != t.shape {
+			panic('All shapes must be equal')
+		}
+	}
+}
