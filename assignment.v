158aad5f722adbc681cd19936e175d560accb527
ulises-jeremias <ulisescf.24@gmail.com>
2020-11-15 23:01:59 -0300

    Updated

--- assignment.v
+++ assignment.v
@@ -3,13 +3,12 @@ module vtl
 // set copies a scalar value into a Tensor at the provided index
 [inline]
 pub fn (mut t Tensor) set(index []int, val voidptr) {
-	mut offset := t.offset(index)
+	offset := t.offset(index)
 	unsafe {t.data.set(offset, val)}
 }
 
 // fill fills an entire Tensor with a given value
 [inline]
 pub fn (mut t Tensor) fill(val voidptr) {
-	// @todo: use iterator instead
 	t.data.fill(val)
 }
--- lookup.v
+++ lookup.v
@@ -3,7 +3,7 @@ module vtl
 // get returns a scalar value from a Tensor at the provided index
 [inline]
 pub fn (t Tensor) get(index []int) voidptr {
-	mut offset := t.offset(index)
+	offset := t.offset(index)
 	unsafe {
 		return t.data.get(offset)
 	}
@@ -14,7 +14,7 @@ pub fn (t Tensor) get(index []int) voidptr {
 [inline]
 pub fn (t Tensor) offset(index []int) int {
 	mut offset := 0
-	for i := 0; i < t.rank(); i++ {
+	for i in 0 .. t.rank() {
 		mut j := index[i]
 		if j < 0 {
 			j += t.shape[i]
