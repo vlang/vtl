0e735e841929a26113ff1bf8be459d3d9e8846cf
ulises-jeremias <ulisescf.24@gmail.com>
2020-11-23 03:22:25 -0300

    Added util func

--- lookup.v
+++ lookup.v
@@ -1,5 +1,7 @@
 module vtl
 
+import math
+
 // get returns a scalar value from a Tensor at the provided index
 [inline]
 [unsafe]
@@ -10,7 +12,7 @@ pub fn (t Tensor) get(index []int) voidptr {
 	}
 }
 
-// offset returns a pointer to a Tensor's data at
+// offset returns the index to a Tensor's data at
 // a given index
 [inline]
 pub fn (t Tensor) offset(index []int) int {
@@ -21,6 +23,20 @@ pub fn (t Tensor) offset(index []int) int {
 			j += t.shape[i]
 		}
 		offset += j * t.strides[i]
+	}
+	return offset
+}
+
+// stride offset returns the index of the starting offset
+// for arrays that may be negatively strided
+pub fn (t Tensor) strided_offset(shape []int, strides []int) int {
+	mut offset := 0
+	for i in 0 .. t.rank() {
+		if strides[i] < 0 {
+			unsafe {
+				offset += (shape[i] - 1) * int(math.abs(strides[i]))
+			}
+		}
 	}
 	return offset
 }
