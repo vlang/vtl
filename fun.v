4699091c8f78bafcdfa1761a793b3ae02478e3c0
ulises-jeremias <ulisescf.24@gmail.com>
2020-12-02 02:12:35 -0300

    Updated files

--- fun.v
+++ fun.v
@@ -26,26 +26,6 @@ pub fn (t Tensor) apply(f ApplyFn) {
 	}
 }
 
-// tensor_to_varray<T> returns the flatten representation of a tensor in a v array storing
-// elements of type T
-pub fn tensor_to_varray<T>(t Tensor) []T {
-	mut arr := []T{}
-	mut iter := t.iterator()
-	for _ in 0 .. t.size {
-		arr << num_as_type<T>(iter.next())
-	}
-	return arr
-}
-
-// tensor_as_type<T> returns a new Tensor with a cast to a given type
-pub fn tensor_as_type<T>(t Tensor) Tensor {
-	arr := tensor_to_varray<T>(t)
-	return new_tensor_from_varray<T>(arr, {
-		shape: t.shape
-		memory: t.memory
-	})
-}
-
 // diagonal returns a view of the diagonal entries
 // of a two dimensional tensor
 pub fn (t Tensor) diagonal() Tensor {
