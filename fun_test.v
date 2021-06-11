34eecb8f56a167c36899e240b8315d7f31efb31e
ulises-jeremias <ulisescf.24@gmail.com>
2021-06-11 17:40:32 -0300

    Updated slice function

--- fun_test.v
+++ fun_test.v
@@ -26,31 +26,31 @@ fn test_transpose() {
 	assert tt.shape == [3, 2]
 }
 
-// fn test_slice() {
-// 	a := from_varray([0, 1, 2, 3, 4, 5, 6, 7, 8], [3, 3])
-// 	slice := a.slice([0])
-// 	expected := from_varray([0, 1, 2], [3])
-// 	assert slice.equal(expected)
-// }
+fn test_slice() {
+	a := from_varray([0., 1, 2, 3, 4, 5, 6, 7, 8], [3, 3])
+	slice := a.slice([0])
+	expected := from_varray([0., 1, 2], [3])
+	assert slice.equal(expected)
+}
 
-// fn test_slice_implicit() {
-// 	a := from_varray([0, 1, 2, 3], [2, 2])
-// 	slice := a.slice([]int{}, [1])
-// 	expected := from_varray([1, 3], [2])
-// 	assert slice.equal(expected)
-// }
+fn test_slice_implicit() {
+	a := from_varray([0., 1, 2, 3], [2, 2])
+	slice := a.slice([]int{}, [1])
+	expected := from_varray([1., 3], [2])
+	assert slice.equal(expected)
+}
 
 // fn test_negative_slice() {
-// 	a := from_varray([1, 2, 3], [3])
+// 	a := from_varray([1., 2, 3], [3])
 // 	slice := a.slice([0, 3, -1])
-// 	expected := from_varray([3, 2, 1], [3])
+// 	expected := from_varray([3., 2, 1], [3])
 // 	assert slice.equal(expected)
 // }
 
-// fn test_slice_hilo() {
-//     t := from_varray([1., 2, 3, 4], [2, 2])
-//     slice := t.slice_hilo([0], [2])
-// 	println(t)
-// 	println(slice)
-//     assert t.equal(slice)
-// }
+fn test_slice_hilo() {
+    t := from_varray([1., 2, 3, 4], [2, 2])
+    slice := t.slice_hilo([0], [2])
+	println(t)
+	println(slice)
+    assert t.equal(slice)
+}
