dc81d97c3aab292d4245b5c2da7bb53f5f47ff9f
ulises-jeremias <ulisescf.24@gmail.com>
2021-08-15 21:33:38 -0300

    fix?

--- assignment.v
+++ assignment.v
@@ -18,11 +18,12 @@ pub fn (t Tensor<T>) fill<T>(val T) {
 // assign sets the values of an Tensor equal to the values of another
 // Tensor of the same shape
 pub fn (mut t Tensor<T>) assign<T>(other &Tensor<T>) {
-	mut iters := t.iterators<T>([other])
-	mut pos := iters[0].pos
+	mut iters := iterators<T>([t, other])
+	mut t_iter := iters[0]
+	mut pos := t_iter.pos
 	for {
 		vals := iterators_next<T>(mut iters) or { break }
 		storage.storage_set<T>(t.data, pos, vals[1])
-		pos = iters[0].pos
+		pos = t_iter.pos
 	}
 }
