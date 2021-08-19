11d523ba60076702d821f9cb9c0e42aaf07ebcca
ulises-jeremias <ulisescf.24@gmail.com>
2021-08-19 03:05:25 -0300

    Updated example

--- examples/mnist.v
+++ examples/mnist.v
@@ -3,11 +3,12 @@ module main
 import vtl.datasets
 
 fn main() {
-	mut mnist_loader := datasets.load_mnist(.train, 32) or { panic(err) }
+	mut mnist_loader := datasets.load_mnist(.test, 32) or { panic(err) }
 	println(mnist_loader)
 
+	mut i := 0
 	for batch in mnist_loader {
-		println(batch.str())
-		break
+		println('Batch number: ${i++}')
+		// println(batch.str())
 	}
 }
