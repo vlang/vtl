793c3da2c7c4744ea976f63e7ae8d0358f34e7e8
ulises-jeremias <ulisescf.24@gmail.com>
2021-09-03 23:59:38 -0300

    Updated iter

--- iter.v
+++ iter.v
@@ -31,22 +31,6 @@ pub fn (t &Tensor<T>) iterator<T>() &TensorIterator<T> {
 	return t.strided_iterator()
 }
 
-fn (t &Tensor<T>) rowmajor_contiguous_iterator<T>() &TensorIterator<T> {
-	return t.custom_iterator<T>(
-		next_handler: .flatten_iteration
-	)
-}
-
-fn (t &Tensor<T>) strided_iterator<T>() &TensorIterator<T> {
-	coord := []int{len: t.rank()}
-	return t.custom_iterator<T>(
-		coord: coord
-		backstrides: tensor_backstrides<T>(t)
-		next_handler: .strided_iteration
-		pos: t.strided_offset_index()
-	)
-}
-
 pub struct IteratorBuildData<T> {
 	next_handler IteratorStrategy
 	coord        []int
@@ -63,6 +47,91 @@ pub fn (t &Tensor<T>) custom_iterator<T>(data IteratorBuildData<T>) &TensorItera
 		pos: data.pos
 		next_handler: data.next_handler
 	}
+}
+
+// next calls the iteration type for a given iterator
+// which is either flat or strided and returns a Num containing the current value
+[inline]
+pub fn (mut s TensorIterator<T>) next<T>() ?(T, int) {
+	if s.iteration >= s.tensor.size() {
+		return none
+	}
+	defer {
+		s.iteration++
+	}
+	pos := s.pos
+	val := if s.next_handler == .flatten_iteration {
+		handle_flatten_iteration<T>(mut s)
+	} else {
+		handle_strided_iteration<T>(mut s)
+	}
+	return val, pos
+}
+
+// iterators creates an array of iterators through a list of tensors
+pub fn (t &Tensor<T>) iterators<T>(ts []&Tensor<T>) []&TensorIterator<T> {
+	mut next_ts := [t]
+	for t_ in ts {
+		next_ts << t_
+	}
+	return iterators<T>(next_ts)
+}
+
+// iterators creates an array of iterators through a list of tensors
+[inline]
+pub fn (ts []&Tensor<T>) iterators<T>() []&TensorIterator<T> {
+	return iterators<T>(ts)
+}
+
+// iterators creates an array of iterators through a list of tensors
+fn iterators<T>(ts []&Tensor<T>) []&TensorIterator<T> {
+	if ts.len == 0 {
+		return []&TensorIterator<T>{}
+	}
+	mut iters := []&TensorIterator<T>{cap: ts.len}
+	for i in 0 .. ts.len {
+		tib := ts[i].broadcast_to<T>(ts[0].shape)
+		iters << tib.iterator<T>()
+	}
+	return iters
+}
+
+// next calls the iteration type for a given list of iterators
+// which is either flat or strided and returns a list of Nums containing the current values
+[inline]
+pub fn (mut its []&TensorIterator<T>) next<T>() ?([]T, int) {
+	return iterators_next<T>(mut its)
+}
+
+// iterators_next calls the iteration type for a given list of iterators
+// which is either flat or strided and returns a list of Nums containing the current values
+fn iterators_next<T>(mut its []&TensorIterator<T>) ?([]T, int) {
+	mut nums := []T{cap: its.len}
+	mut pos := -1
+	for i, mut iter in its {
+		val, pos_ := iter.next() or { return err }
+		if i == 0 {
+			pos = pos_
+		}
+		nums << T(val)
+	}
+	return nums, pos
+}
+
+fn (t &Tensor<T>) rowmajor_contiguous_iterator<T>() &TensorIterator<T> {
+	return t.custom_iterator<T>(
+		next_handler: .flatten_iteration
+	)
+}
+
+fn (t &Tensor<T>) strided_iterator<T>() &TensorIterator<T> {
+	coord := []int{len: t.rank()}
+	return t.custom_iterator<T>(
+		coord: coord
+		backstrides: tensor_backstrides<T>(t)
+		next_handler: .strided_iteration
+		pos: t.strided_offset_index()
+	)
 }
 
 // handle_strided_iteration advances through a non-rowmajor-contiguous
@@ -93,38 +162,13 @@ fn handle_strided_iteration<T>(mut s TensorIterator<T>) T {
 
 // handle_flatten_iteration advances through a rowmajor-contiguous Tensor
 // in Row-Major order
-pub fn handle_flatten_iteration<T>(mut s TensorIterator<T>) T {
+fn handle_flatten_iteration<T>(mut s TensorIterator<T>) T {
 	// get current value after update new position
 	val := storage.storage_get<T>(s.tensor.data, s.pos)
 	defer {
 		s.pos++
 	}
 	return val
-}
-
-// next calls the iteration type for a given iterator
-// which is either flat or strided and returns a Num containing the current value
-[inline]
-pub fn (mut s TensorIterator<T>) next<T>() ?(T, int) {
-	return iterator_next<T>(mut s)
-}
-
-// iterator_next calls the iteration type for a given iterator
-// which is either flat or strided and returns a Num containing the current value
-pub fn iterator_next<T>(mut s TensorIterator<T>) ?(T, int) {
-	if s.iteration >= s.tensor.size() {
-		return none
-	}
-	defer {
-		s.iteration++
-	}
-	pos := s.pos
-	val := if s.next_handler == .flatten_iteration {
-		handle_flatten_iteration<T>(mut s)
-	} else {
-		handle_strided_iteration<T>(mut s)
-	}
-	return val, pos
 }
 
 fn tensor_backstrides<T>(t &Tensor<T>) []int {
@@ -136,54 +180,4 @@ fn tensor_backstrides<T>(t &Tensor<T>) []int {
 		backstrides[i] = strides[i] * (shape[i] - 1)
 	}
 	return backstrides
-}
-
-// iterators creates an array of iterators through a list of tensors
-pub fn (t &Tensor<T>) iterators<T>(ts []&Tensor<T>) []&TensorIterator<T> {
-	mut next_ts := [t]
-	for t_ in ts {
-		next_ts << t_
-	}
-	return iterators<T>(next_ts)
-}
-
-// iterators creates an array of iterators through a list of tensors
-[inline]
-pub fn (ts []&Tensor<T>) iterators<T>() []&TensorIterator<T> {
-	return iterators<T>(ts)
-}
-
-// iterators creates an array of iterators through a list of tensors
-pub fn iterators<T>(ts []&Tensor<T>) []&TensorIterator<T> {
-	if ts.len == 0 {
-		return []&TensorIterator<T>{}
-	}
-	mut iters := []&TensorIterator<T>{cap: ts.len}
-	for i in 0 .. ts.len {
-		tib := ts[i].broadcast_to<T>(ts[0].shape)
-		iters << tib.iterator<T>()
-	}
-	return iters
-}
-
-// next calls the iteration type for a given list of iterators
-// which is either flat or strided and returns a list of Nums containing the current values
-[inline]
-pub fn (mut its []&TensorIterator<T>) next<T>() ?([]T, int) {
-	return iterators_next<T>(mut its)
-}
-
-// iterators_next calls the iteration type for a given list of iterators
-// which is either flat or strided and returns a list of Nums containing the current values
-pub fn iterators_next<T>(mut its []&TensorIterator<T>) ?([]T, int) {
-	mut nums := []T{cap: its.len}
-	mut pos := -1
-	for i, mut iter in its {
-		val, pos_ := iter.next() or { return err }
-		if i == 0 {
-			pos = pos_
-		}
-		nums << T(val)
-	}
-	return nums, pos
 }
