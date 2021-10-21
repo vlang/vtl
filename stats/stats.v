a6cbba7835a9611bec369d09619702fea2f36b8c
ulises-jeremias <ulisescf.24@gmail.com>
2021-10-21 01:36:53 -0300

    Added stats submodule

---
+++ stats/stats.v
@@ -0,0 +1,241 @@
+module stats
+
+import vtl
+import math.stats
+
+// Measure of Occurance
+// Frequency of a given number
+// Based on
+// https://www.mathsisfun.com/data/frequency-distribution.html
+pub fn freq<T>(t &vtl.Tensor<T>, val T) int {
+	return stats.freq<T>(t.to_array(), val)
+}
+
+// Measure of Central Tendancy
+// Mean of the given input array
+// Based on
+// https://www.mathsisfun.com/data/central-measures.html
+pub fn mean<T>(t &vtl.Tensor<T>) T {
+	return stats.mean<T>(t.to_array())
+}
+
+// Measure of Central Tendancy
+// Geometric Mean of the given input array
+// Based on
+// https://www.mathsisfun.com/numbers/geometric-mean.html
+pub fn geometric_mean<T>(t &vtl.Tensor<T>) T {
+	return stats.geometric_mean<T>(t.to_array())
+}
+
+// Measure of Central Tendancy
+// Harmonic Mean of the given input array
+// Based on
+// https://www.mathsisfun.com/numbers/harmonic-mean.html
+pub fn harmonic_mean<T>(t &vtl.Tensor<T>) T {
+	return stats.harmonic_mean<T>(t.to_array())
+}
+
+// Measure of Central Tendancy
+// Median of the given input array ( input array is assumed to be sorted )
+// Based on
+// https://www.mathsisfun.com/data/central-measures.html
+pub fn median<T>(t &vtl.Tensor<T>) T {
+	return stats.median<T>(t.to_array())
+}
+
+// Measure of Central Tendancy
+// Mode of the given input array
+// Based on
+// https://www.mathsisfun.com/data/central-measures.html
+pub fn mode<T>(t &vtl.Tensor<T>) T {
+	return stats.mode<T>(t.to_array())
+}
+
+// Root Mean Square of the given input array
+// Based on
+// https://en.wikipedia.org/wiki/Root_mean_square
+pub fn rms<T>(t &vtl.Tensor<T>) T {
+	return stats.rms<T>(t.to_array())
+}
+
+// Measure of Dispersion / Spread
+// Population Variance of the given input array
+// Based on
+// https://www.mathsisfun.com/data/standard-deviation.html
+[inline]
+pub fn population_variance<T>(t &vtl.Tensor<T>) T {
+	return stats.population_variance<T>(t.to_array())
+}
+
+// Measure of Dispersion / Spread
+// Population Variance of the given input array
+// Based on
+// https://www.mathsisfun.com/data/standard-deviation.html
+pub fn population_variance_mean<T>(t &vtl.Tensor<T>, mean T) T {
+	return stats.population_variance_mean<T>(t.to_array(), mean)
+}
+
+// Measure of Dispersion / Spread
+// Sample Variance of the given input array
+// Based on
+// https://www.mathsisfun.com/data/standard-deviation.html
+[inline]
+pub fn sample_variance<T>(t &vtl.Tensor<T>) T {
+	return stats.sample_variance<T>(t.to_array())
+}
+
+// Measure of Dispersion / Spread
+// Sample Variance of the given input array
+// Based on
+// https://www.mathsisfun.com/data/standard-deviation.html
+pub fn sample_variance_mean<T>(t &vtl.Tensor<T>, mean T) T {
+	return stats.sample_variance_mean<T>(t.to_array(), mean)
+}
+
+// Measure of Dispersion / Spread
+// Population Standard Deviation of the given input array
+// Based on
+// https://www.mathsisfun.com/data/standard-deviation.html
+[inline]
+pub fn population_stddev<T>(t &vtl.Tensor<T>) T {
+	return stats.population_stddev<T>(t.to_array())
+}
+
+// Measure of Dispersion / Spread
+// Population Standard Deviation of the given input array
+// Based on
+// https://www.mathsisfun.com/data/standard-deviation.html
+[inline]
+pub fn population_stddev_mean<T>(t &vtl.Tensor<T>, mean T) T {
+	return stats.population_stddev_mean<T>(t.to_array(), mean)
+}
+
+// Measure of Dispersion / Spread
+// Sample Standard Deviation of the given input array
+// Based on
+// https://www.mathsisfun.com/data/standard-deviation.html
+[inline]
+pub fn sample_stddev<T>(t &vtl.Tensor<T>) T {
+	return stats.sample_stddev<T>(t.to_array())
+}
+
+// Measure of Dispersion / Spread
+// Sample Standard Deviation of the given input array
+// Based on
+// https://www.mathsisfun.com/data/standard-deviation.html
+[inline]
+pub fn sample_stddev_mean<T>(t &vtl.Tensor<T>, mean T) T {
+	return stats.sample_stddev_mean<T>(t.to_array(), mean)
+}
+
+// Measure of Dispersion / Spread
+// Mean Absolute Deviation of the given input array
+// Based on
+// https://en.wikipedia.org/wiki/Average_absolute_deviation
+[inline]
+pub fn absdev<T>(t &vtl.Tensor<T>) T {
+	return stats.absdev<T>(t.to_array())
+}
+
+// Measure of Dispersion / Spread
+// Mean Absolute Deviation of the given input array
+// Based on
+// https://en.wikipedia.org/wiki/Average_absolute_deviation
+pub fn absdev_mean<T>(t &vtl.Tensor<T>, mean T) T {
+	return stats.absdev_mean<T>(t.to_array(), mean)
+}
+
+// Sum of squares
+[inline]
+pub fn tss<T>(t &vtl.Tensor<T>) T {
+	return stats.tss<T>(t.to_array())
+}
+
+// Sum of squares about the mean
+pub fn tss_mean<T>(t &vtl.Tensor<T>, mean T) T {
+	return stats.tss_mean<T>(t.to_array(), mean)
+}
+
+// Minimum of the given input array
+pub fn min<T>(t &vtl.Tensor<T>) T {
+	return stats.min<T>(t.to_array())
+}
+
+// Maximum of the given input array
+pub fn max<T>(t &vtl.Tensor<T>) T {
+	return stats.max<T>(t.to_array())
+}
+
+// Minimum and maximum of the given input array
+pub fn minmax<T>(t &vtl.Tensor<T>) (T, T) {
+	return stats.minmax<T>(t.to_array())
+}
+
+// Minimum of the given input array
+pub fn min_index<T>(t &vtl.Tensor<T>) int {
+	return stats.min_index<T>(t.to_array())
+}
+
+// Maximum of the given input array
+pub fn max_index<T>(t &vtl.Tensor<T>) int {
+	return stats.max_index<T>(t.to_array())
+}
+
+// Minimum and maximum of the given input array
+pub fn minmax_index<T>(t &vtl.Tensor<T>) (int, int) {
+	return stats.minmax_index<T>(t.to_array())
+}
+
+// Measure of Dispersion / Spread
+// Range ( Maximum - Minimum ) of the given input array
+// Based on
+// https://www.mathsisfun.com/data/range.html
+pub fn range<T>(t &vtl.Tensor<T>) T {
+	return stats.range<T>(t.to_array())
+}
+
+[inline]
+pub fn covariance<T>(a &vtl.Tensor<T>, b &vtl.Tensor<T>) T {
+	return stats.covariance<T>(a.to_array(), b.to_array())
+}
+
+// Compute the covariance of a dataset using
+// the recurrence relation
+pub fn covariance_mean<T>(a &vtl.Tensor<T>, b &vtl.Tensor<T>, mean1 T, mean2 T) T {
+	return stats.covariance_mean<T>(a.to_array(), b.to_array(), mean1, mean2)
+}
+
+[inline]
+pub fn lag1_autocorrelation<T>(t &vtl.Tensor<T>) T {
+	return stats.lag1_autocorrelation<T>(t.to_array())
+}
+
+// Compute the lag-1 autocorrelation of a dataset using
+// the recurrence relation
+pub fn lag1_autocorrelation_mean<T>(t &vtl.Tensor<T>, mean T) T {
+	return stats.lag1_autocorrelation_mean<T>(t.to_array(), mean)
+}
+
+[inline]
+pub fn kurtosis<T>(t &vtl.Tensor<T>) T {
+	return stats.kurtosis<T>(t.to_array())
+}
+
+// Takes a dataset and finds the kurtosis
+// using the fourth moment the deviations, normalized by the sd
+pub fn kurtosis_mean_stddev<T>(t &vtl.Tensor<T>, mean T, sd T) T {
+	return stats.kurtosis_mean_stddev<T>(t.to_array(), mean, sd)
+}
+
+[inline]
+pub fn skew<T>(t &vtl.Tensor<T>) T {
+	return stats.skew<T>(t.to_array())
+}
+
+pub fn skew_mean_stddev<T>(t &vtl.Tensor<T>, mean T, sd T) T {
+	return stats.skew_mean_stddev<T>(t.to_array(), mean, sd)
+}
+
+pub fn quantile<T>(sorted_t &vtl.Tensor<T>, f T) T {
+	return stats.quantile<T>(sorted_t.to_array(), f)
+}
---
+++ stats/stats_test.v
@@ -0,0 +1,271 @@
+module stats
+
+import vtl
+import math
+
+fn test_freq() {
+	// Tests were also verified on Wolfram Alpha
+	data := vtl.from_1d([10.0, 10.0, 5.9, 2.7])
+	mut o := freq(data, 10.0)
+	assert o == 2
+	o = freq(data, 2.7)
+	assert o == 1
+	o = freq(data, 15)
+	assert o == 0
+}
+
+fn tst_res(str1 string, str2 string) bool {
+	if (math.abs(str1.f64() - str2.f64())) < 1e-5 {
+		return true
+	}
+	return false
+}
+
+fn test_mean() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := mean(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '5.762500')
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = mean(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '17.650000')
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = mean(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '37.708000')
+}
+
+fn test_geometric_mean() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := geometric_mean(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '5.15993')
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = geometric_mean(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	ok := o.str() == 'nan' || o.str() == '-nan' || o.str() == '-1.#IND00' || o == f64(0)
+		|| o.str() == '-nan(ind)'
+	assert ok // Because in math it yields a complex number
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = geometric_mean(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '25.064496')
+}
+
+fn test_harmonic_mean() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := harmonic_mean(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '4.626519')
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = harmonic_mean(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '9.134577')
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = harmonic_mean(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '16.555477')
+}
+
+fn test_median() {
+	// Tests were also verified on Wolfram Alpha
+	// Assumes sorted array
+
+	// Even
+	mut data := vtl.from_1d([f64(2.7), f64(4.45), f64(5.9), f64(10.0)])
+	mut o := median(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '5.175000')
+	data = vtl.from_1d([f64(-3.0), f64(1.89), f64(4.4), f64(67.31)])
+	o = median(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '3.145000')
+	data = vtl.from_1d([f64(7.88), f64(12.0), f64(54.83), f64(76.122)])
+	o = median(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '33.415000')
+
+	// Odd
+	data = vtl.from_1d([f64(2.7), f64(4.45), f64(5.9), f64(10.0), f64(22)])
+	o = median(data)
+	assert o == f64(5.9)
+	data = vtl.from_1d([f64(-3.0), f64(1.89), f64(4.4), f64(9), f64(67.31)])
+	o = median(data)
+	assert o == f64(4.4)
+	data = vtl.from_1d([f64(7.88), f64(3.3), f64(12.0), f64(54.83), f64(76.122)])
+	o = median(data)
+	assert o == f64(12.0)
+}
+
+fn test_mode() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(2.7), f64(2.7), f64(4.45), f64(5.9), f64(10.0)])
+	mut o := mode(data)
+	assert o == f64(2.7)
+	data = vtl.from_1d([f64(-3.0), f64(1.89), f64(1.89), f64(1.89), f64(9), f64(4.4), f64(4.4), f64(9),
+		f64(67.31)])
+	o = mode(data)
+	assert o == f64(1.89)
+	// Testing greedy nature
+	data = vtl.from_1d([f64(2.0), f64(4.0), f64(2.0), f64(4.0)])
+	o = mode(data)
+	assert o == f64(2.0)
+}
+
+fn test_rms() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := rms(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '6.362046')
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = rms(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '33.773393')
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = rms(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '47.452561')
+}
+
+fn test_population_variance() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := population_variance(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '7.269219')
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = population_variance(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '829.119550')
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = population_variance(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '829.852282')
+}
+
+fn test_sample_variance() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := sample_variance(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '9.692292')
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = sample_variance(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '1105.492733')
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = sample_variance(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '1106.469709')
+}
+
+fn test_population_stddev() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := population_stddev(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '2.696149')
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = population_stddev(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '28.794436')
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = population_stddev(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '28.807157')
+}
+
+fn test_sample_stddev() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := sample_stddev(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '3.113245')
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = sample_stddev(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '33.248951')
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = sample_stddev(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '33.263639')
+}
+
+fn test_absdev() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := absdev(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '2.187500')
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = absdev(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '24.830000')
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = absdev(data)
+	// Some issue with precision comparison in f64 using == operator hence serializing to string
+	assert tst_res(o.str(), '27.768000')
+}
+
+fn test_min() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := min(data)
+	assert o == f64(2.7)
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = min(data)
+	assert o == f64(-3.0)
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = min(data)
+	assert o == f64(7.88)
+}
+
+fn test_max() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := max(data)
+	assert o == f64(10.0)
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = max(data)
+	assert o == f64(67.31)
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = max(data)
+	assert o == f64(76.122)
+}
+
+fn test_range() {
+	// Tests were also verified on Wolfram Alpha
+	mut data := vtl.from_1d([f64(10.0), f64(4.45), f64(5.9), f64(2.7)])
+	mut o := range(data)
+	assert o == f64(7.3)
+	data = vtl.from_1d([f64(-3.0), f64(67.31), f64(4.4), f64(1.89)])
+	o = range(data)
+	assert o == f64(70.31)
+	data = vtl.from_1d([f64(12.0), f64(7.88), f64(76.122), f64(54.83)])
+	o = range(data)
+	assert o == f64(68.242)
+}
+
+fn test_passing_empty() {
+	data := vtl.from_1d([]f64{})
+	assert freq(data, 0) == 0
+	assert mean(data) == f64(0)
+	assert geometric_mean(data) == f64(0)
+	assert harmonic_mean(data) == f64(0)
+	assert median(data) == f64(0)
+	assert mode(data) == f64(0)
+	assert rms(data) == f64(0)
+	assert population_variance(data) == f64(0)
+	assert sample_variance(data) == f64(0)
+	assert population_stddev(data) == f64(0)
+	assert sample_stddev(data) == f64(0)
+	assert absdev(data) == f64(0)
+	assert min(data) == f64(0)
+	assert max(data) == f64(0)
+	assert range(data) == f64(0)
+}
