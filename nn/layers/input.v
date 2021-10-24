473350aa838284e087ec4621dc3dd29efa068a2f
ulises-jeremias <ulisescf.24@gmail.com>
2021-10-24 16:20:15 -0300

    Added input layer gate

---
+++ nn/layers/input.v
@@ -0,0 +1,18 @@
+module layers
+
+import vtl
+import vtl.autograd
+
+pub struct InputGate<T> {}
+
+pub fn (g &InputGate<T>) backward<T>(payload &Payload<T>) []&vtl.Tensor<T> {
+	gradient := payload.variable.grad
+	return [gradient]
+}
+
+pub fn (g &InputGate<T>) cache<T>(mut result Variable<T>, args ...autograd.CacheParam) {
+	result.grad = vtl.zeros_like<T>(result.value)
+	result.requires_grad = true
+
+	register<T>('Input', g, result, ...args)
+}
