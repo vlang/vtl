4a9f2307ffe2f8313b92f48474b4f2ea27efe0d4
ulises-jeremias <ulisescf.24@gmail.com>
2021-11-20 18:54:54 -0300

    Added interfaces

---
+++ nn/layer.v
@@ -0,0 +1,10 @@
+module nn
+
+import vtl.autograd
+
+// Layer is a generic interface for a neural network layer.
+pub interface Layer<T> {
+        output_shape []int
+        variables []&autograd.Variable<T>
+        forward(input &autograd.Variable<T>)
+}
