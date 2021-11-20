11dcf7baf6ac635ebeffebf9e03e96086a4086c9
ulises-jeremias <ulisescf.24@gmail.com>
2021-11-20 18:54:54 -0300

    Added interfaces

---
+++ nn/loss.v
@@ -0,0 +1,9 @@
+module nn
+
+import vtl
+import vtl.autograd
+
+// Loss is a generic interface for loss functions.
+pub interface Loss<T> {
+        loss(input &autograd.Variable<T>, target &vtl.Tensor<T>) &autograd.Variable<T>
+}
