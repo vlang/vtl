94b4706e2e334425e1b1a641be34e972f2d4a08e
ulises-jeremias <ulisescf.24@gmail.com>
2021-12-19 02:23:43 -0300

    Updated examples

---
+++ datasets/examples/imdb.v
@@ -0,0 +1,19 @@
+module main
+
+import vtl.datasets
+
+fn main() {
+	mut train_ds := datasets.load_imdb(.train, batch_size: 6) ?
+
+	// mut i := 0
+	// for {
+	// 	batch := train_ds.next() or { break }
+	// 	println('Labels Batch #${i++}')
+	// 	println(*batch.labels)
+	// 	println('')
+
+	// 	if i == 10 {
+	// 		break
+	// 	}
+	// }
+}
