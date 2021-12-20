3025ec80767830cf16a728c42a2361537b564189
ulises-jeremias <ulisescf.24@gmail.com>
2021-12-20 03:03:10 -0300

    Updated types

---
+++ datasets/types.v
@@ -0,0 +1,23 @@
+module datasets
+
+import vtl
+
+// DatasetType is the type of a dataset.
+pub enum DatasetType {
+	train
+	test
+}
+
+// DatasetLoader is an interface for loading datasets.
+pub interface DatasetLoader {
+	@type DatasetType
+	batch_size int
+mut:
+	next() ?DatasetBatch
+}
+
+// @todo: Use generic type for DatasetBatch
+pub interface DatasetBatch {
+	labels &vtl.Tensor<int>
+	features &vtl.Tensor<string>
+}
