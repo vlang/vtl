ba8cf5cf5db3e18b618859164f02e9d8549286bd
ulises-jeremias <ulisescf.24@gmail.com>
2022-02-12 18:00:54 -0300

    Added dist functions

--- rand.v
+++ rand.v
@@ -1,14 +1,50 @@
 module vtl
 
 import rand
+import rand.dist
 import rand.seed
 import time
 
+// bernoulli returns a tensor of bernoulli random variables.
+pub fn bernoulli<T>(prob f64, shape []int, params TensorData) &Tensor<T> {
+        mut t := zeros<T>(shape, params)
+	mut iter := t.iterator()
+	for {
+		_, pos := iter.next() or { break }
+		rand_value := T(dist.bernoulli(prob))
+		t.data.set<T>(pos, rand_value)
+	}
+	return t
+}
+
+// binomial returns a tensor of binomial random variables.
+pub fn binomial<T>(n int, prob f64, shape []int, params TensorData) &Tensor<T> {
+        mut t := zeros<T>(shape, params)
+	mut iter := t.iterator()
+	for {
+		_, pos := iter.next() or { break }
+		rand_value := T(dist.binomial(n, prob))
+		t.data.set<T>(pos, rand_value)
+	}
+	return t
+}
+
+// exponential returns a tensor of exponential random variables.
+pub fn exponential<T>(lambda f64, shape []int, params TensorData) &Tensor<T> {
+        mut t := zeros<T>(shape, params)
+	mut iter := t.iterator()
+	for {
+		_, pos := iter.next() or { break }
+		rand_value := T(dist.exponential(lambda))
+		t.data.set<T>(pos, rand_value)
+	}
+	return t
+}
+
 // random returns a new Tensor of given shape and type, initialized
 // with random numbers between a given min and max value
-[inline]
 pub fn random<T>(min T, max T, shape []int, params TensorData) &Tensor<T> {
-	mut t := empty<T>(shape, params)
+	mut t := zeros<T>(shape, params)
 	mut iter := t.iterator()
 	for {
 		_, pos := iter.next() or { break }
@@ -20,6 +56,11 @@ pub fn random<T>(min T, max T, shape []int, params TensorData) &Tensor<T> {
 
 pub fn random_seed(i int) {
 	rand.seed(seed.time_seed_array(2))
+}
+
+fn init() {
+	unix_time := u32(time.now().unix)
+	rand.seed([unix_time, 0])
 }
 
 fn random_in_range<T>(min T, max T) T {
@@ -51,9 +92,4 @@ fn random_in_range<T>(min T, max T) T {
 		return rand.f64_in_range(min, max)
 	}
 	return T(0)
-}
-
-fn init() {
-	unix_time := u32(time.now().unix)
-	rand.seed([unix_time, 0])
 }
