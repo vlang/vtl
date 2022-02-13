4ee9484f9108b986906fc206928d08f95ca07b3f
ulises-jeremias <ulisescf.24@gmail.com>
2022-02-13 18:18:23 -0300

    Updated container

--- nn/nn.v
+++ nn/nn.v
@@ -3,29 +3,30 @@ module nn
 import vtl.autograd
 import vtl.nn.layers
 
-pub struct NNLayers<T> {
+pub struct NeuralNetworkContainer<T> {
 	ctx &autograd.Context<T>
 pub mut:
 	layers []Layer
+        loss Loss
 }
 
-// new_nn_layers creates a new neural network container
+// new_nnc creates a new neural network container
 // with an empty list of layers.
-pub fn new_nn_layers<T>(ctx &autograd.Context<T>) &NNLayers<T> {
-	return &NNLayers<T>{
+pub fn new_nnc<T>(ctx &autograd.Context<T>) &NeuralNetworkContainer<T> {
+	return &NeuralNetworkContainer<T>{
 		ctx: ctx
 	}
 }
 
 // input adds a new input layer to the network
 // with the given shape.
-pub fn (mut ls NNLayers<T>) input(shape []int) {
+pub fn (mut ls NeuralNetworkContainer<T>) input(shape []int) {
 	ls.layers << layers.new_input_layer(ls.ctx, shape)
 }
 
 // linear adds a new linear layer to the network
 // with the given output size
-pub fn (mut ls NNLayers<T>) linear(output_size int) {
+pub fn (mut ls NeuralNetworkContainer<T>) linear(output_size int) {
 	// input_size := ls.layers[ls.layers.len - 1].output_shape()[0]
 	input_size := output_size
 	ls.layers << layers.new_linear_layer<T>(ls.ctx, input_size, output_size)
--- nn/nn_test.v
+++ nn/nn_test.v
@@ -4,6 +4,6 @@ import vtl.autograd
 
 fn test_nn() {
 	mut ctx := autograd.new_ctx<f64>()
-	mut nncon := new_nn_layers<f64>(ctx)
+	mut nncon := new_nnc<f64>(ctx)
 	nncon.input([1, 2])
 }
