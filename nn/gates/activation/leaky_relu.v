7eba48be7271fa3cefb49fac112ff53c0bd80cc5
ulises-jeremias <ulisescf.24@gmail.com>
2022-02-13 18:04:52 -0300

    Updated layers names

--- nn/gates/activation/leaky_relu.v
+++ nn/gates/activation/leaky_relu.v
@@ -4,25 +4,25 @@ import vtl
 import vtl.autograd
 import vtl.nn.internal
 
-pub struct LeakyReluGate<T> {
+pub struct LeakyEluGate<T> {
 pub:
 	cache &vtl.Tensor<T>
 }
 
-pub fn new_leaky_relu_gate<T>(cache &vtl.Tensor<T>) &LeakyReluGate<T> {
-	return &LeakyReluGate<T>{
+pub fn new_leaky_relu_gate<T>(cache &vtl.Tensor<T>) &LeakyEluGate<T> {
+	return &LeakyEluGate<T>{
 		cache: cache
 	}
 }
 
-pub fn (g &LeakyReluGate<T>) backward<T>(payload &autograd.Payload<T>) []&vtl.Tensor<T> {
+pub fn (g &LeakyEluGate<T>) backward<T>(payload &autograd.Payload<T>) []&vtl.Tensor<T> {
 	gradient := payload.variable.grad
 	return [internal.deriv_leaky_relu(gradient, g.cache)]
 }
 
-pub fn (g &LeakyReluGate<T>) cache<T>(mut result autograd.Variable<T>, args ...autograd.CacheParam) {
+pub fn (g &LeakyEluGate<T>) cache<T>(mut result autograd.Variable<T>, args ...autograd.CacheParam) {
 	result.grad = vtl.zeros_like<T>(result.value)
 	result.requires_grad = true
 
-	register<T>('LeakyRelu', g, result, ...args)
+	register<T>('LeakyElu', g, result, ...args)
 }
--- nn/layers/relu.v
+++ nn/layers/relu.v
@@ -5,26 +5,26 @@ import vtl.autograd
 import vtl.nn.internal
 import vtl.nn.gates.layers
 
-// ReluLayer is a layer that applies the rectified linear unit function element-wise.
-pub struct ReluLayer<T> {
+// ReLULayer is a layer that applies the rectified linear unit function element-wise.
+pub struct ReLULayer<T> {
 	output_shape []int
 }
 
-pub fn new_relu_layer<T>(ctx &autograd.Context<T>, output_shape []int) &ReluLayer<T> {
-	return &ReluLayer<T>{
+pub fn new_relu_layer<T>(ctx &autograd.Context<T>, output_shape []int) &ReLULayer<T> {
+	return &ReLULayer<T>{
 		output_shape: output_shape.clone()
 	}
 }
 
-pub fn (layer &ReluLayer<T>) output_shape() []int {
+pub fn (layer &ReLULayer<T>) output_shape() []int {
 	return layer.output_shape
 }
 
-pub fn (_ &ReluLayer<T>) variables() []&autograd.Variable<T> {
+pub fn (_ &ReLULayer<T>) variables() []&autograd.Variable<T> {
 	return []&autograd.Variable<T>{}
 }
 
-pub fn (layer &ReluLayer<T>) forward(mut input autograd.Variable<T>) &autograd.Variable<T> {
+pub fn (layer &ReLULayer<T>) forward(mut input autograd.Variable<T>) &autograd.Variable<T> {
 	output := internal.relu<T>(input.value)
 	mut result := input.context.variable(output)
 
