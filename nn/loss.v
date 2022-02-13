0acc1075275faa45f6b5d6adf22bdbe1022a65cf
ulises-jeremias <ulisescf.24@gmail.com>
2022-02-13 18:18:23 -0300

    Updated container

--- nn/loss.v
+++ nn/loss.v
@@ -4,6 +4,6 @@ import vtl
 import vtl.autograd
 
 // Loss is a generic interface for loss functions.
-pub interface Loss<T> {
-	loss(input &autograd.Variable<T>, target &vtl.Tensor<T>) &autograd.Variable<T>
+pub interface Loss {
+	// loss(input &autograd.Variable<T>, target &vtl.Tensor<T>) &autograd.Variable<T>
 }
