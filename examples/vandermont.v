97343b58d96c480a1ac236d7de87a464660545ce
ulises-jeremias <ulisescf.24@gmail.com>
2022-07-07 03:06:35 -0300

    Updated example

--- examples/vandermont.v
+++ examples/vandermont.v
@@ -19,8 +19,31 @@ t := vtl.from_2d(vandermont)
 
 println(t)
 
-println(t.shape)
+println(t.shape) // [5, 5]
 
-slice := t.slice_hilo([1, 3], [3, 5])
+println('slice: ')
+slice1 := t.slice_hilo([2, 3], [4, 5])
 
-println(slice)
+println(slice1)
+// [[16, 32], [81, 243]]
+
+println(slice1.shape) // [2, 2]
+
+slice2 := t.slice_hilo([3], [])
+
+println('span slice: ')
+println(slice2)
+// [[   4,   16,   64,  256, 1024],
+// [   5,   25,  125,  625, 3125]]
+
+println(slice2.shape) // [2, 5]
+
+slice3 := t.slice_hilo([], [-2])
+
+println('slice until: ')
+println(slice3)
+// [[  1,   1,   1,   1,   1],
+// [  2,   4,   8,  16,  32],
+// [  3,   9,  27,  81, 243]]
+
+println(slice3.shape) // [3, 5]
