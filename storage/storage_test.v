10fe581d638ba78de194487f04658d9742afb9a5
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-16 02:08:30 -0300

    Added comp time flag

--- storage/storage_test.v
+++ storage/storage_test.v
@@ -10,19 +10,21 @@ fn test_cpu_with_default() {
 }
 
 fn test_vcl_with_default() {
-	// get all devices if you want
-	devices := vcl.get_devices(vcl.device_cpu) or { panic(err) }
-	println('Devices: $devices')
+	$if vcl ? {
+		// get all devices if you want
+		devices := vcl.get_devices(vcl.device_cpu) or { panic(err) }
+		println('Devices: $devices')
 
-	// do not create platforms/devices/contexts/queues/...
-	// just get the device
-	mut device := vcl.get_default_device() or { panic(err) }
-	defer {
-		device.release() or { panic(err) }
+		// do not create platforms/devices/contexts/queues/...
+		// just get the device
+		mut device := vcl.get_default_device() or { panic(err) }
+		defer {
+			device.release() or { panic(err) }
+		}
+
+		s := new_storage<f64>(2, 0, 1.0).vcl(device: device) or { panic(err) }
+		array := s.to_array() or { panic(err) }
+		assert array.len == 2
+		assert array[0] == 1.0
 	}
-
-	s := new_storage<f64>(2, 0, 1.0).vcl(device: device) or { panic(err) }
-	array := s.to_array() or { panic(err) }
-	assert array.len == 2
-	assert array[0] == 1.0
 }
--- storage/types.v
+++ storage/types.v
@@ -2,5 +2,3 @@ module storage
 
 // StorageDataType is a sum type that lists the possible types to be used to define storage
 pub type StorageDataType = byte | f32 | f64 | i16 | i64 | i8 | int | u16 | u32 | u64
-
-pub type Storage<T> = CpuStorage<T> | VclStorage<T>
