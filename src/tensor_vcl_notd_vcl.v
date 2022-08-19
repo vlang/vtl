f869a190d0190826962710fef9a47193a03248bb
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-19 03:22:50 -0300

    Updated type

--- src/tensor_vcl_notd_vcl.v
+++ src/tensor_vcl_notd_vcl.v
@@ -1,7 +1,7 @@
 module vtl
 
 // vcl returns a VclTensor from a Tensor
-pub fn (t &Tensor<T>) vcl(params _VAnonStruct1) ?&Tensor<T> {
+pub fn (t &Tensor<T>) vcl(params struct {}) ?&Tensor<T> {
 	return error(@STRUCT + '.' + @FN + ':' +
 		' it is needed to compile with the flag "-d vcl" to use the VCL Backend')
 }
