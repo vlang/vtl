3cf62ea105245af69f9089f4e9dd494ccfe96d67
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-23 02:27:18 -0300

    Updated examples

--- autograd/examples/autograd.v
+++ autograd/examples/autograd.v
@@ -6,15 +6,15 @@ import vtl.autograd
 fn main() {
 	ctx := autograd.new_ctx<f64>()
 
-	x := ctx.variable(vtl.from_1d([3.0]))
-	y := ctx.variable(vtl.from_1d([2.0]))
+	x := ctx.variable(vtl.from_1d([3.0])?)
+	y := ctx.variable(vtl.from_1d([2.0])?)
 
 	println(x)
 	println(y)
 
-	mut f := autograd.pow(x, y)
+	mut f := autograd.pow(x, y)?
 
-	f.backprop()
+	f.backprop()?
 
 	println(f)
 	println(x.grad)
