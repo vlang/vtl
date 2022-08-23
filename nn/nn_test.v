7deb53072737bb02e9afff126641a07834169851
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-23 03:54:30 -0300

    Updated test

--- nn/nn_test.v
+++ nn/nn_test.v
@@ -7,6 +7,9 @@ fn test_nnc() {
 	mut nncon := new_nnc<f64>(ctx)
 	nncon.input([1, 2])
 	nncon.sigmod()
+        assert nncon.layers.len == 2
+        assert nncon.layers[0].output_shape() == [1, 2]
+        assert nncon.layers[1].output_shape() == [1, 2]
 }
 
 fn test_nn() {
