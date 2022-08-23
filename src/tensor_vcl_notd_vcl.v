1b9ba6c26337fc31c14caeba0155d575e5fc26d2
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-23 02:44:23 -0300

    Updated vcl backend to match new protocol

--- src/tensor_vcl_notd_vcl.v
+++ src/tensor_vcl_notd_vcl.v
@@ -1,7 +1,10 @@
 module vtl
 
+[params]
+pub struct VclParams {}
+
 // vcl returns a VclTensor from a Tensor
-pub fn (t &Tensor<T>) vcl(params struct {}) ?&Tensor<T> {
-	return error(@STRUCT + '.' + @METHOD + ':' +
+pub fn (t &Tensor<T>) vcl(params VclParams) ?&Tensor<T> {
+	return error(@METHOD + ':' +
 		' it is needed to compile with the flag "-d vcl" to use the VCL Backend')
 }
