0ede1e0fb88b6e0cf5220c9f4cd640af99e5d3e6
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-23 21:03:03 -0300

    Updated adam optimizer

--- nn/optimizers/adam.v
+++ nn/optimizers/adam.v
@@ -1,5 +1,6 @@
 module optimizers
 
+import math
 import vtl.autograd
 import vtl.nn.types
 import vtl
@@ -45,6 +46,36 @@ pub fn (mut o AdamOptimizer<T>) build_params(layers []types.Layer) {
 	}
 }
 
-pub fn (mut o AdamOptimizer<T>) update() {
-	// @todo: @ulises-jeremias to implement this
+pub fn (mut o AdamOptimizer<T>) update() ? {
+	lr_t := o.learning_rate * math.sqrt(1.0 - o.beta2_t) / (1.0 - o.beta1_t)
+
+	o.beta1_t *= o.beta1
+	o.beta2_t *= o.beta2
+
+	for i, mut v in o.params {
+		if v.requires_grad {
+			mut fm_iters := o.first_moments[i].iterators([v.grad])?
+			for {
+				vals, idx := vtl.iterators_next<T>(mut fm_iters) or { break }
+				val := o.beta1 * vals[0] + (1.0 - o.beta1) * vals[1]
+				o.first_moments[i].set(idx, T(val))
+			}
+
+			mut sm_iters := o.second_moments[i].iterators([v.grad])?
+			for {
+				vals, idx := vtl.iterators_next<T>(mut sm_iters) or { break }
+				val := o.beta2 * vals[0] + (1.0 - o.beta2) * vals[1] * vals[1]
+				o.second_moments[i].set(idx, T(val))
+			}
+
+			mut val_iters := v.value.iterators([o.first_moments[i], o.second_moments[i]])?
+			for {
+				vals, idx := vtl.iterators_next<T>(mut val_iters) or { break }
+				val := vals[0] - lr_t * vals[1] / (math.sqrt(vals[3]) + o.epsilon)
+				v.value.set(idx, T(val))
+			}
+
+			v.grad = vtl.zeros_like<T>(v.value)
+		}
+	}
 }
