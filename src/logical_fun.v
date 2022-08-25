c6a17c1ebc92ffdb45e02277527d675641ec335a
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-25 03:19:26 -0300

    Updated iterator

--- src/logical_fun.v
+++ src/logical_fun.v
@@ -1,5 +1,7 @@
 module vtl
 
+import arrays
+
 // array_equal returns true if input arrays have the same shape and all elements equal.
 pub fn (t &Tensor<T>) array_equal<T>(other &Tensor<T>) bool {
 	if t.shape != other.shape {
@@ -27,4 +29,28 @@ pub fn (t &Tensor<T>) array_equiv<T>(other &Tensor<T>) bool {
 		}
 	}
 	return true
+}
+
+[inline]
+fn handle_equal<T>(vals []T, _ []int) bool {
+	mut equal := true
+	for v in vals {
+		equal = equal && v == vals[0]
+	}
+	return equal
+}
+
+// equal adds two tensors elementwise
+[inline]
+pub fn (t &Tensor<T>) equal<T>(other &Tensor<T>) ?&Tensor<bool> {
+	// @todo: Implement using t.nmap
+	// return t.nmap<T>(handle_equal, other)
+	mut iters, shape := t.iterators<T>([other])?
+	mut ret := empty<bool>(shape)
+	for {
+		vals, i := iterators_next<T>(mut iters) or { break }
+		val := handle_equal<T>(vals, i)
+		ret.set(i, val)
+	}
+	return ret
 }
