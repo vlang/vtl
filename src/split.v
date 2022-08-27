eb4d4fdd92eacb6c7e8922f19839f162143cbaa7
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-27 01:34:57 -0300

    Updated functions to methods

--- src/split.v
+++ src/split.v
@@ -6,7 +6,7 @@ module vtl
 // integer that does not equally divide the axis. For an array of length
 // l that should be split into n sections, it returns l % n sub-arrays of
 // size l//n + 1 and the rest of size l//n.
-pub fn array_split<T>(t &Tensor<T>, ind int, axis int) ?[]&Tensor<T> {
+pub fn (t &Tensor<T>) array_split<T>(ind int, axis int) ?[]&Tensor<T> {
 	ntotal := t.shape[axis]
 	neach := ntotal / ind
 	extras := ntotal % ind
@@ -19,7 +19,7 @@ pub fn array_split<T>(t &Tensor<T>, ind int, axis int) ?[]&Tensor<T> {
 		rt += sizes[i]
 		sizes[i] += tmp
 	}
-	return splitter<T>(t, axis, ind, sizes)
+	return t.splitter<T>(axis, ind, sizes)
 }
 
 // array_split_expl splits an array into multiple sub-arrays.
@@ -28,23 +28,23 @@ pub fn array_split<T>(t &Tensor<T>, ind int, axis int) ?[]&Tensor<T> {
 // integer that does not equally divide the axis. For an array of length
 // l that should be split into n sections, it returns l % n sub-arrays of
 // size l//n + 1 and the rest of size l//n.
-pub fn array_split_expl<T>(t &Tensor<T>, ind []int, axis int) ?[]&Tensor<T> {
+pub fn (t &Tensor<T>) array_split_expl<T>(ind []int, axis int) ?[]&Tensor<T> {
 	nsections := ind.len + 1
 	mut div_points := [0]
 	div_points << ind
 	div_points << [t.shape[axis]]
-	return splitter<T>(t, axis, nsections, div_points)
+	return t.splitter<T>(axis, nsections, div_points)
 }
 
 // split splits an array into multiple sub-arrays. The array will be divided into
 // N equal arrays along axis. If such a split is not possible,
 // panic
-pub fn split<T>(t &Tensor<T>, ind int, axis int) ?[]&Tensor<T> {
+pub fn (t &Tensor<T>) split<T>(ind int, axis int) ?[]&Tensor<T> {
 	n := t.shape[axis]
 	if n % ind != 0 {
 		return error('Array split does not result in an equal division')
 	}
-	return array_split<T>(t, ind, axis)
+	return t.array_split<T>(ind, axis)
 }
 
 // split_expl splits an array into multiple sub-arrays. The array will be divided into
@@ -53,18 +53,18 @@ pub fn split<T>(t &Tensor<T>, ind int, axis int) ?[]&Tensor<T> {
 // ary[:2]
 // ary[2:3]
 // ary[3:]
-pub fn split_expl<T>(t &Tensor<T>, ind []int, axis int) []&Tensor<T> {
-	return array_split_expl<T>(t, ind, axis)
+pub fn (t &Tensor<T>) split_expl<T>(ind []int, axis int) ?[]&Tensor<T> {
+	return t.array_split_expl<T>(ind, axis)
 }
 
 // hsplit splits an array into multiple sub-arrays horizontally (column-wise).
 // Please refer to the split documentation. hsplit is equivalent to
 // split with axis=1, the array is always split along the second axis
 // regardless of the array dimension.
-pub fn hsplit<T>(t &Tensor<T>, ind int) []&Tensor<T> {
+pub fn (t &Tensor<T>) hsplit<T>(ind int) ?[]&Tensor<T> {
 	return match t.rank() {
-		1 { split<T>(t, ind, 0) }
-		else { split<T>(t, ind, 1) }
+		1 { t.split<T>(ind, 0)? }
+		else { t.split<T>(ind, 1)? }
 	}
 }
 
@@ -72,10 +72,10 @@ pub fn hsplit<T>(t &Tensor<T>, ind int) []&Tensor<T> {
 // Please refer to the split documentation. hsplit is equivalent to
 // split with axis=1, the array is always split along the second axis
 // regardless of the array dimension.
-pub fn hsplit_expl<T>(t &Tensor<T>, ind []int) []&Tensor<T> {
+pub fn (t &Tensor<T>) hsplit_expl<T>(ind []int) ?[]&Tensor<T> {
 	return match t.rank() {
-		1 { split_expl<T>(t, ind, 0) }
-		else { split_expl<T>(t, ind, 1) }
+		1 { t.split_expl<T>(ind, 0)? }
+		else { t.split_expl<T>(ind, 1)? }
 	}
 }
 
@@ -83,49 +83,49 @@ pub fn hsplit_expl<T>(t &Tensor<T>, ind []int) []&Tensor<T> {
 // Please refer to the split documentation. vsplit is equivalent to
 // split with axis=0 (default), the array is always split along the
 // first axis regardless of the array dimension.
-pub fn vsplit<T>(t &Tensor<T>, ind int) ?[]&Tensor<T> {
+pub fn (t &Tensor<T>) vsplit<T>(ind int) ?[]&Tensor<T> {
 	if t.rank() < 2 {
 		return error('vsplit only works on tensors of >= 2 dimensions')
 	}
-	return split<T>(t, ind, 0)
+	return t.split<T>(ind, 0)
 }
 
 // vsplit_expl splits an array into multiple sub-arrays vertically (row-wise).
 // Please refer to the split documentation. vsplit is equivalent to
 // split with axis=0 (default), the array is always split along the
 // first axis regardless of the array dimension.
-pub fn vsplit_expl<T>(t &Tensor<T>, ind []int) ?[]&Tensor<T> {
+pub fn (t &Tensor<T>) vsplit_expl<T>(ind []int) ?[]&Tensor<T> {
 	if t.rank() < 2 {
 		return error('vsplit only works on tensors of >= 2 dimensions')
 	}
-	return split_expl<T>(t, ind, 0)
+	return t.split_expl<T>(ind, 0)
 }
 
 // dsplit splits array into multiple sub-arrays along the 3rd axis (depth).
 // Please refer to the split documentation. dsplit is equivalent to
 // split with axis=2, the array is always split along the third axis
 // provided the array dimension is greater than or equal to 3.
-pub fn dsplit<T>(t &Tensor<T>, ind int) ?[]&Tensor<T> {
+pub fn (t &Tensor<T>) dsplit<T>(ind int) ?[]&Tensor<T> {
 	if t.rank() < 3 {
 		return error('dsplit only works on arrays of 3 or more dimensions')
 	}
-	return split<T>(t, ind, 2)
+	return t.split<T>(ind, 2)
 }
 
 // dsplit_expl splits array into multiple sub-arrays along the 3rd axis (depth).
 // Please refer to the split documentation. dsplit is equivalent to
 // split with axis=2, the array is always split along the third axis
 // provided the array dimension is greater than or equal to 3.
-pub fn dsplit_expl<T>(t &Tensor<T>, ind []int) ?[]&Tensor<T> {
+pub fn (t &Tensor<T>) dsplit_expl<T>(ind []int) ?[]&Tensor<T> {
 	if t.rank() < 3 {
 		return error('dsplit only works on arrays of 3 or more dimensions')
 	}
-	return split_expl<T>(t, ind, 2)
+	return t.split_expl<T>(ind, 2)
 }
 
 // splitter implements a generic splitting function that contains the underlying functionality
 // for all split operations
-fn splitter<T>(t &Tensor<T>, axis int, n int, div_points []int) ?[]&Tensor<T> {
+fn (t &Tensor<T>) splitter<T>(axis int, n int, div_points []int) ?[]&Tensor<T> {
 	mut subary := []&Tensor<T>{}
 	sary := t.swapaxes(axis, 0)?
 	for i in 0 .. n {
