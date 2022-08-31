5b137d31a774df13f46f66b0c963800323b086b2
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-31 00:53:09 -0300

    Added absolute error functions

--- ml/metrics/common_error_functions.v
+++ ml/metrics/common_error_functions.v
@@ -13,3 +13,13 @@ pub fn squared_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> 
 pub fn mean_squared_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> {
 	return stats.mean(squared_error(y, y_true))
 }
+
+[inline]
+pub fn absolute_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> {
+	return y_true.substract(y).abs()
+}
+
+[inline]
+pub fn mean_absolute_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> {
+	return stats.mean(absolute_error(y, y_true))
+}
