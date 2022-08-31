412d8e97e7aaa84461610ef0e92f72594f40d515
ulises-jeremias <ulisescf.24@gmail.com>
2022-08-31 01:15:00 -0300

    Updated metrics

--- ml/metrics/common_error_functions.v
+++ ml/metrics/common_error_functions.v
@@ -1,36 +1,48 @@
 module metrics
 
+import math
 import vtl
 import vtl.stats
 
 [inline]
-pub fn squared_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> {
-	diff := y.substract(y_true)
+pub fn squared_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) ?&vtl.Tensor<T> {
+	diff := y.substract(y_true)?
 	return diff.multiply(diff)
 }
 
 [inline]
-pub fn mean_squared_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> {
-	return stats.mean(squared_error(y, y_true))
+pub fn mean_squared_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) ?T {
+	return stats.mean<T>(squared_error<T>(y, y_true)?)
 }
 
 [inline]
-pub fn relative_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> {
-	denom := y_true.abs().max(y.abs())
-	return y_true.substract(y).abs().divide(denom)
+pub fn relative_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) ?&vtl.Tensor<T> {
+	mut iters, shape := y.iterators<T>([y_true])?
+	mut ret := vtl.new_tensor_like_with_shape<T>(y, shape)
+	for {
+		vals, i := iters.next() or { break }
+		denom := math.max(math.abs(vals[1]), math.abs(vals[0]))
+		val := if denom == vtl.new_t<T>(0) {
+			vtl.new_t<T>(0)
+		} else {
+			math.abs(math.abs(vals[1]) - math.abs(vals[0])) / denom
+		}
+		ret.set(i, val)
+	}
+	return ret
 }
 
 [inline]
-pub fn mean_relative_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> {
-	return stats.mean(relative_error(y, y_true))
+pub fn mean_relative_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) ?T {
+	return stats.mean<T>(relative_error<T>(y, y_true)?)
 }
 
 [inline]
-pub fn absolute_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> {
-	return y_true.substract(y).abs()
+pub fn absolute_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) ?&vtl.Tensor<T> {
+	return y_true.substract(y)?.abs()
 }
 
 [inline]
-pub fn mean_absolute_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) &vtl.Tensor<T> {
-	return stats.mean(absolute_error(y, y_true))
+pub fn mean_absolute_error<T>(y &vtl.Tensor<T>, y_true &vtl.Tensor<T>) ?T {
+	return stats.mean<T>(absolute_error<T>(y, y_true)?)
 }
