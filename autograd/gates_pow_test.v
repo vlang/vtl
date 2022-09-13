fe7bdd7b8c16f9b0b0909deeacd9737abcddcb01
ulises-jeremias <ulisescf.24@gmail.com>
2022-09-13 02:30:11 -0300

    Fix

--- autograd/gates_pow_test.v
+++ autograd/gates_pow_test.v
@@ -8,7 +8,7 @@ fn test_pow() {
 	x := ctx.variable(vtl.from_1d([3.0]) or { panic(@FN + ' failed') })
 	y := ctx.variable(vtl.from_1d([2.0]) or { panic(@FN + ' failed') })
 
-	mut f := pow(x, y) or { panic(@FN + ' failed') }
+	mut f := x.pow(y) or { panic(@FN + ' failed') }
 
 	f.backprop() or { panic(@FN + ' failed') }
 
