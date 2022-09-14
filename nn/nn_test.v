f1c67432d36dbd1b9a49a2a4cc8753c9527ddbbf
ulises-jeremias <ulisescf.24@gmail.com>
2022-09-14 00:32:29 -0300

    Updated nn

--- nn/nn_test.v
+++ nn/nn_test.v
@@ -5,12 +5,12 @@ import vtl.nn.layers
 
 fn test_nnc() {
 	ctx := autograd.new_ctx<f64>()
-	mut nncon := new_nnc<f64>(ctx)
-	nncon.input([1, 2])
-	nncon.sigmod()
-	assert nncon.layers.len == 2
-	assert layers.layer_output_shape<f64>(nncon.layers[0]) == [1, 2]
-	assert layers.layer_output_shape<f64>(nncon.layers[1]) == [1, 2]
+	mut nn := new_nn<f64>(ctx)
+	nn.input([1, 2])
+	nn.sigmod()
+	assert nn.info.layers.len == 2
+	assert layers.layer_output_shape<f64>(nn.info.layers[0]) == [1, 2]
+	assert layers.layer_output_shape<f64>(nn.info.layers[1]) == [1, 2]
 }
 
 fn test_nn() {
