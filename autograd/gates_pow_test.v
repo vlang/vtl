19f3e055f9a3f73dd4650d1aa0f4b0ecb225cac2
ulises-jeremias <ulisescf.24@gmail.com>
2022-09-19 17:02:00 -0300

    Updated tests

--- autograd/gates_pow_test.v
+++ autograd/gates_pow_test.v
@@ -2,15 +2,15 @@ module autograd
 
 import vtl
 
-fn test_pow() {
+fn test_pow() ? {
 	f64_ctx := ctx<f64>()
 
-	x := f64_ctx.variable(vtl.from_1d([3.0]) or { panic(@FN + ' failed') })
-	y := f64_ctx.variable(vtl.from_1d([2.0]) or { panic(@FN + ' failed') })
+	x := f64_ctx.variable(vtl.from_1d([3.0])?)
+	y := f64_ctx.variable(vtl.from_1d([2.0])?)
 
-	mut f := x.pow(y) or { panic(@FN + ' failed') }
+	mut f := x.pow(y)?
 
-	f.backprop() or { panic(@FN + ' failed') }
+	f.backprop()?
 
-	assert x.grad.array_equal(vtl.from_1d([6.0]) or { panic(@FN + ' failed') })
+	assert x.grad.array_equal(vtl.from_1d([6.0])?)
 }
--- src/broadcast_test.v
+++ src/broadcast_test.v
@@ -1,23 +1,21 @@
 module vtl
 
-fn test_broadcast_column() {
-	m := from_array([1.0, 2.0, 3.0], [3, 1]) or { panic(@FN + ' failed') }
-	b := m.broadcast_to([3, 3]) or { panic(@FN + ' failed') }
-	expected := from_array([1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 3.0, 3.0, 3.0], [3, 3]) or {
-		panic(@FN + ' failed')
-	}
+fn test_broadcast_column() ? {
+	m := from_array([1.0, 2.0, 3.0], [3, 1])?
+	b := m.broadcast_to([3, 3])?
+	expected := from_array([1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 3.0, 3.0, 3.0], [3, 3])?
 	assert b.array_equal(expected)
 }
 
-fn test_broadcastable_same_shape() {
-	m := from_array([1.0, 2.0, 3.0, 4.0], [2, 2]) or { panic(@FN + ' failed') }
-	shape := m.broadcastable(m) or { panic(@FN + ' failed') }
+fn test_broadcastable_same_shape() ? {
+	m := from_array([1.0, 2.0, 3.0, 4.0], [2, 2])?
+	shape := m.broadcastable(m)?
 	assert m.shape == shape
 }
 
-fn test_broadcastable_different_shape() {
+fn test_broadcastable_different_shape() ? {
 	a := zeros<f64>([8, 1, 6, 1])
 	b := zeros<f64>([7, 1, 5])
-	shape := a.broadcastable(b) or { panic(@FN + ' failed') }
+	shape := a.broadcastable(b)?
 	assert shape == [8, 7, 6, 5]
 }
