22f35d57cb7f3e1e0e4a74bf63c475a33f7b3cda
ulises-jeremias <ulisescf.24@gmail.com>
2022-09-30 00:22:12 -0300

    Updated tests

---
+++ la/vlas_test.v
@@ -0,0 +1,28 @@
+module la
+
+import vtl
+
+fn test_matmul1() ? {
+	a := vtl.from_2d([[1.0, 0], [0.0, 1]])?
+	b := vtl.from_2d([[4.0, 1], [2.0, 2]])?
+	expected := vtl.from_2d([[4.0, 1], [2.0, 2]])?
+	result := matmul(a, b)?
+	assert result.shape == [2, 2]
+	assert result.array_equal(expected)
+}
+
+// fn test_matmul2() ? {
+// 	a := vtl.seq<f64>(2 * 2 * 4).reshape([2, 2, 4])?
+// 	b := vtl.seq<f64>(2 * 2 * 4).reshape([2, 4, 2])?
+// 	result := matmul(a, b)?
+// 	assert result.shape == [2, 2, 2]
+// }
+
+// fn test_matmul3() ? {
+// 	a := vtl.from_2d([[1.0, 0], [0.0, 1]])?
+// 	b := vtl.from_1d([1.0, 2])?
+// 	expected := vtl.from_1d([1.0, 2])?
+// 	result := matmul(a, b)?
+// 	assert result.shape == [1, 2]
+// 	assert result.array_equal(expected)
+// }
