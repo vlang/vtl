f56f68868b2ae26642a93e4bee5c540d1520edab
ulises-jeremias <ulisescf.24@gmail.com>
2022-10-07 16:32:09 -0300

    Fix docs

--- datasets/examples/mnist.v
+++ datasets/examples/mnist.v
@@ -2,18 +2,16 @@ module main
 
 import vtl.datasets
 
-fn main() {
-	mut train_ds := datasets.load_mnist(.train, batch_size: 6)?
+mut train_ds := datasets.load_mnist(.train, batch_size: 6)?
 
-	mut i := 0
-	for {
-		batch := train_ds.next() or { break }
-		println('Labels Batch #${i++}')
-		println(*batch.labels)
-		println('')
+mut i := 0
+for {
+        batch := train_ds.next() or { break }
+        println('Labels Batch #${i++}')
+        println(*batch.labels)
+        println('')
 
-		if i == 10 {
-			break
-		}
-	}
+        if i == 10 {
+                break
+        }
 }
