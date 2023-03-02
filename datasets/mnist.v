5538feb28db24065129fc4f0add76a5d9dc989ef
yuyi <yuyi98@163.com>
2023-03-02 16:21:19 +0800

    fmt mnist.v

--- datasets/mnist.v
+++ datasets/mnist.v
@@ -94,7 +94,9 @@ pub fn (mut ds MnistDataset) next() ?MnistBatch {
 		lft.set(i, 1)
 	}
 
-	ft := (vtl.from_array(features, [features.len]) or { return none }).reshape([-1, 1, 32, 32]) or { return none }
+	ft := (vtl.from_array(features, [features.len]) or { return none }).reshape([-1, 1, 32, 32]) or {
+		return none
+	}
 
 	return MnistBatch{
 		labels: lft
