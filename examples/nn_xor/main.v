66effcf07fcc058079121409cb3a5dd60c6599b6
ulises-jeremias <ulisescf.24@gmail.com>
2023-06-17 16:02:46 -0300

    Updated examples and docs

--- examples/nn_xor/main.v
+++ examples/nn_xor/main.v
@@ -7,7 +7,7 @@ import vtl.nn.optimizers
 
 const (
 	batch_size = 32
-	epochs     = 50
+	epochs     = 6
 	batches    = 100
 )
 
@@ -17,7 +17,7 @@ const (
 ctx := autograd.ctx[f64]()
 
 // We will create a tensor of size 3200 (100 batches of size 32)
-// We create it as int between [0, 2[ and convert to bool
+// We create it as int between [0, 2] and convert to bool
 x_train_bool := vtl.random(0, 2, [batch_size * 100, 2]).as_bool()
 
 // Let's build our truth labels. We need to apply xor between the 2 columns of the tensors
@@ -26,7 +26,9 @@ x_train_bool_2 := x_train_bool.slice_hilo([]int{}, [1])!
 y_bool := x_train_bool_1.equal(x_train_bool_2)!
 
 // We need to convert the bool tensor to a float tensor
-mut x_train := ctx.variable(x_train_bool.as_f64())
+mut x_train := ctx.variable(x_train_bool.as_f64(),
+	requires_grad: true
+)
 y := y_bool.as_f64()
 
 // We create a neural network with 2 inputs, 2 hidden layers of 4 neurons each and 1 output
@@ -39,12 +41,13 @@ model.linear(1)
 model.sigmoid_cross_entropy_loss()
 
 // Stochastic Gradient Descent
-mut optimizer := optimizers.sgd[f64](learning_rate: 0.7)
+mut optimizer := optimizers.sgd[f64](learning_rate: 0.01)
 
 mut losses := []&vtl.Tensor[f64]{cap: epochs * batches}
 
 // Learning loop
 for epoch in 0 .. epochs {
+	println('Epoch: ${epoch}')
 	for batch_id in 0 .. batches {
 		// minibatch offset in the Tensor
 		offset := batch_id * batch_size
