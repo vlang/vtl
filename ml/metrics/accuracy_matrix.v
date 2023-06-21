60b6a6a1d8203a80a3976701858c3823d6580bec
ulises-jeremias <ulisescf.24@gmail.com>
2023-06-21 03:15:09 -0300

    Updated la methods

--- ml/metrics/accuracy_matrix.v
+++ ml/metrics/accuracy_matrix.v
@@ -3,7 +3,7 @@ module metrics
 import vtl
 import math.stats
 
-// accuracy_score returns the proportion of correctly classified samples (as f32)
+// accuracy_score returns the proportion of correctly classified samples (as f64)
 pub fn accuracy_score[T](y_pred &vtl.Tensor[T], y_true &vtl.Tensor[T]) !f64 {
 	equal := y_pred.equal(y_true)!
 	equal_arr := equal.to_array[bool]().map(if it { 1.0 } else { 0.0 })
