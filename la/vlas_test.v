c3572990f68c78b761d0bd25950d8f3070c18e61
ulises-jeremias <ulisescf.24@gmail.com>
2023-06-21 02:30:13 -0300

    Added tests for tensor methods

--- la/vlas_test.v
+++ la/vlas_test.v
@@ -11,18 +11,18 @@ fn test_matmul1() {
 	assert result.array_equal(expected)
 }
 
-// fn test_matmul2() ? {
-// 	a := vtl.seq[f64](2 * 2 * 4).reshape([2, 2, 4])?
-// 	b := vtl.seq[f64](2 * 2 * 4).reshape([2, 4, 2])?
-// 	result := matmul(a, b)?
+// fn test_matmul2() {
+// 	a := vtl.seq[f64](2 * 2 * 4).reshape([2, 2, 4])!
+// 	b := vtl.seq[f64](2 * 2 * 4).reshape([2, 4, 2])!
+// 	result := matmul(a, b)!
 // 	assert result.shape == [2, 2, 2]
 // }
 
-// fn test_matmul3() ? {
-// 	a := vtl.from_2d([[1.0, 0], [0.0, 1]])?
-// 	b := vtl.from_1d([1.0, 2])?
-// 	expected := vtl.from_1d([1.0, 2])?
-// 	result := matmul(a, b)?
+// fn test_matmul3() {
+// 	a := vtl.from_2d([[1.0, 0], [0.0, 1]])!
+// 	b := vtl.from_1d([1.0, 2])!
+// 	expected := vtl.from_1d([1.0, 2])!
+// 	result := matmul(a, b)!
 // 	assert result.shape == [1, 2]
 // 	assert result.array_equal(expected)
 // }
