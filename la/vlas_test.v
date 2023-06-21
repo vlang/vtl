8b51693669b7d089a097aa856abb113eaaf64d52
ulises-jeremias <ulisescf.24@gmail.com>
2023-06-21 02:36:31 -0300

    Added tests for tensor methods

--- la/vlas_test.v
+++ la/vlas_test.v
@@ -11,12 +11,15 @@ fn test_matmul_1() {
 	assert result.array_equal(expected)
 }
 
-// fn test_matmul_2() {
-// 	a := vtl.seq[f64](2 * 2 * 4).reshape([2, 2, 4])!
-// 	b := vtl.seq[f64](2 * 2 * 4).reshape([2, 4, 2])!
-// 	result := matmul(a, b)!
-// 	assert result.shape == [2, 2, 2]
-// }
+fn test_matmul_2() {
+	a := vtl.seq[f64](2 * 2 * 4).reshape([2, 2, 4])!
+	b := vtl.seq[f64](2 * 2 * 4).reshape([2, 4, 2])!
+	if _ := matmul(a, b) {
+		assert false
+	} else {
+		assert true
+	}
+}
 
 fn test_matmul_3() {
 	a := vtl.from_2d([[1.0, 0], [0.0, 1]])!
