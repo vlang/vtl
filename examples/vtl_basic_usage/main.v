19b1b13f344ae178d10e337c3e3ab692c3356121
ulises-jeremias <ulisescf.24@gmail.com>
2023-06-23 02:43:49 -0300

    Updated fun methods

--- examples/vtl_basic_usage/main.v
+++ examples/vtl_basic_usage/main.v
@@ -3,6 +3,18 @@ import vtl
 a := vtl.from_1d([1, 2, 3, 4])!
 b := vtl.from_1d([0, 1, 2, 3])!
 
-c := a.add(b)!
+mut c := a.add(b)!
 
 println(c)
+
+c.apply(fn (x int, i []int) int {
+	return x * 2
+})
+
+println(c)
+
+d := c.map(fn (x int, i []int) int {
+	return x * 2
+})
+
+println(d)
