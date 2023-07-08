c12e9022d6959947e8752bd4993396f5e38b8d22
ulises-jeremias <ulisescf.24@gmail.com>
2023-07-08 18:10:27 -0300

    Removed unused file

--- storage/types.v
+++
@@ -1,4 +0,0 @@
-module storage
-
-// StorageDataType is a sum type that lists the possible types to be used to define storage
-pub type StorageDataType = f32 | f64 | i16 | i64 | i8 | int | u16 | u32 | u64
