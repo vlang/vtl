1512b06ed8787e3f9354b643c15d0f2d7a1d5452
ulises-jeremias <ulisescf.24@gmail.com>
2023-10-16 23:16:11 -0300

    Updated plot impl

--- examples/vtl_plot_scatter_colorscale/main.v
+++ examples/vtl_plot_scatter_colorscale/main.v
@@ -3,35 +3,36 @@ module main
 import vsl.plot
 import vtl
 
-y := [
-	0.0,
-	1,
-	3,
-	1,
-	0,
-	-1,
-	-3,
-	-1,
-	0,
-	1,
-	3,
-	1,
-	0,
-]
-x := vtl.seq[f64](y.len)
+fn main() {
+	y := [
+		0.0,
+		1,
+		3,
+		1,
+		0,
+		-1,
+		-3,
+		-1,
+		0,
+		1,
+		3,
+		1,
+		0,
+	]
+	x := vtl.seq[f64](y.len)
 
-mut plt := plot.new_plot()
-plt.add_trace(
-	trace_type: .scatter
-	x: x.to_array()
-	y: y
-	mode: 'lines+markers'
-	colorscale: 'smoker'
-	marker: plot.Marker{
-		size: []f64{len: x.size, init: 10.0}
-	}
-)
-plt.set_layout(
-	title: 'Scatter plot example'
-)
-plt.show()!
+	mut plt := plot.Plot.new()
+	plt.scatter(
+		x: x.to_array()
+		y: y
+		mode: 'lines+markers'
+		colorscale: 'smoker'
+		marker: plot.Marker{
+			size: []f64{len: x.size, init: 10.0}
+		}
+	)
+	plt.layout(
+		title: 'Scatter plot example'
+	)
+	plt.show()!
+}
