21a84020ef18e91a09850de3e90f58f2c8399a17
ulises-jeremias <ulisescf.24@gmail.com>
2026-05-12 02:57:47 -0300

    feat: add runtime vulkan path for multi-head attention

--- nn/layers/attention_vulkan_notd_vulkan.v
+++ nn/layers/attention_vulkan_notd_vulkan.v
@@ -6,3 +6,11 @@ import vtl
 pub fn attention_forward_vulkan[T](q &vtl.Tensor[T], k &vtl.Tensor[T], v &vtl.Tensor[T], head_dim int) !&vtl.Tensor[T] {
 	return error('attention_forward_vulkan: not available without -d vulkan flag')
 }
+
+pub fn attention_scores_vulkan[T](q &vtl.Tensor[T], k &vtl.Tensor[T], head_dim int) !&vtl.Tensor[T] {
+	return error('attention_scores_vulkan: not available without -d vulkan flag')
+}
+
+pub fn attention_apply_values_vulkan[T](weights &vtl.Tensor[T], v &vtl.Tensor[T], head_dim int) !&vtl.Tensor[T] {
+	return error('attention_apply_values_vulkan: not available without -d vulkan flag')
+}
